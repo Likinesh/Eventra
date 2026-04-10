c2139838f5b1ed95e8e318404a02dc4091a1b2d0
Likinesh <likithkk2004@gmail.com>
2026-04-11 00:40:33 +0530

    Authentication and DB setup

--- app/layout.js
+++ app/layout.js
@@ -1,45 +1,55 @@
 import { ThemeProvider } from "@/components/theme-provider";
 import "./globals.css";
 import Header from "@/components/Header";
+import { ConvexClientProvider } from "./ConvexClientProvider";
+import { ClerkProvider } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
 
 export const metadata = {
   title: "Spott",
-  description: "Discover and create amazing events with Spott - your ultimate event discovery and creation platform.",
+  description:
+    "Discover and create amazing events with Spott - your ultimate event discovery and creation platform.",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body 
+      <body
         className={`bg-linear-to-br from-gray-950 via-zinc-900 to-stone-900 text-white`}
       >
         <ThemeProvider
-            attribute="class"
-            defaultTheme="system"
-            enableSystem
-            disableTransitionOnChange
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
         >
-          {/* Header */}
-          <Header />
-          
-          <main className="min-h-screen relative container mx-auto pt-40 md:pt-32">
-            {/* Glow for page */}
-            <div className="pointer-events-none absolute inset-0 -z-10 overflow-hidden">
-              <div className="absolute top-0 left-1/4 w-96 h-96 bg-pink-600/20 rounded-full blur-3xl" />
-              <div className="absolute bottom-0 right-1/4 w-96 h-96 bg-pink-600/20 rounded-full blur-3xl" />
-            </div>
+          <ClerkProvider
+            appearance={{
+              theme: dark
+            }}
+          >
+            <ConvexClientProvider>
+              {/* Header */}
+              <Header />
 
-            <div className="relative z-10 h-[70vh]">
-              {children}
-            </div>
+              <main className="min-h-screen relative container mx-auto pt-40 md:pt-32">
+                {/* Glow for page */}
+                <div className="pointer-events-none absolute inset-0 -z-10 overflow-hidden">
+                  <div className="absolute top-0 left-1/4 w-96 h-96 bg-pink-600/20 rounded-full blur-3xl" />
+                  <div className="absolute bottom-0 right-1/4 w-96 h-96 bg-pink-600/20 rounded-full blur-3xl" />
+                </div>
 
-            {/* Footer */}
-            <footer className=" border-t border-gray-800/50 py-8 px-6 max-w-7xl mx-auto">
-              <p className="text-center text-sm text-gray-500">
-                &copy; 2023 Spott. All rights reserved.
-              </p>
-            </footer>
-          </main>
+                <div className="relative z-10 h-[70vh]">{children}</div>
+
+                {/* Footer */}
+                <footer className=" border-t border-gray-800/50 py-8 px-6 max-w-7xl mx-auto">
+                  <p className="text-center text-sm text-gray-500">
+                    &copy; 2026 Spott. All rights reserved.
+                  </p>
+                </footer>
+              </main>
+            </ConvexClientProvider>
+          </ClerkProvider>
         </ThemeProvider>
       </body>
     </html>
--- components/Header.jsx
+++ components/Header.jsx
@@ -1,33 +1,59 @@
-import Image from 'next/image'
-import Link from 'next/link'
-import React from 'react'
+"use client";
+import { SignInButton, SignUpButton, UserButton } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs/server";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import { Button } from "./ui/button";
+import { Authenticated, Unauthenticated } from "convex/react";
+import { BarLoader } from "react-spinners";
+import { useStoreUser } from "@/hooks/user-store-user";
 
 const Header = () => {
+  const { isLoading } = useStoreUser();
   return (
     <>
-         <nav className='fixed top-0 right-0 bg-background/80 backdrop-blur-xl z-20 left-0 border-b'>
-            <div className='max-w-7xl mx-auto px-6 py-4 flex items-center justify-between'>
-                {/* Logo */}
-                <Link href={"/"} className='flex items-center'>
-                    <Image 
-                        src={"/spott.png"} 
-                        alt='Logo' 
-                        width={500} 
-                        height={500}
-                        className='w-full h-11'
-                        priority
-                    />
-                </Link>
-                
-                {/* Location Search */}
+      <nav className="fixed top-0 right-0 bg-background/80 backdrop-blur-xl z-20 left-0 border-b">
+        <div className="max-w-7xl mx-auto px-6 py-4 flex items-center justify-between">
+          {/* Logo */}
+          <Link href={"/"} className="flex items-center">
+            <Image
+              src={"/spott.png"}
+              alt="Logo"
+              width={500}
+              height={500}
+              className="w-full h-11"
+              priority
+            />
+          </Link>
 
-                {/* Side actions */}
-            </div>
+          {/* Location Search */}
 
-            {/* Mobile Actions */}
-         </nav>
+          {/* Side actions */}
+          <div className=" flex items-center">
+            <Authenticated>
+              <UserButton />
+            </Authenticated>
+            <Unauthenticated>
+              {/* <Link href={"/sign-in"}> */}
+              <SignInButton mode="modal">
+                <Button size="sm">Sign In</Button>
+              </SignInButton>
+              {/* </Link> */}
+            </Unauthenticated>
+          </div>
+        </div>
+
+        {/* Mobile Actions */}
+        {/* Loader */}
+        {isLoading && (
+          <div className="absolute bottom-0 left-0 w-full">
+            <BarLoader width={"100%"} color="#a855f7" />
+          </div>
+        )}
+      </nav>
     </>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
